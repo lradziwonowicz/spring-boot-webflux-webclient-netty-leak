97c5ca726fcf835609e9efdd604998f09fd3263d
Lukasz Radziwonowicz <lradziwonowicz@team.ucando.eu>
2019-03-28 10:34:25 +0100

    Initial app setup

---
+++ src/main/java/com/example/springbootwebfluxwebclientnettyleak/SpringBootWebfluxWebclientNettyLeakApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwebfluxwebclientnettyleak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxWebclientNettyLeakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxWebclientNettyLeakApplication.class, args);
+	}
+
+}
